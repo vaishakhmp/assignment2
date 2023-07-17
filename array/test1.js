809670c75965894acb597dbab6523cabe31ac9e5
vaishakhmp <125654696+vaishakhmp@users.noreply.github.com>
2023-07-17 15:55:55 +0530

    test

---
+++ array/test1.js
@@ -0,0 +1,7 @@
+const marks = [95, 86, 66, 94, 58];
+var average = 0;
+for (let i=0; i<marks.length; i++){
+    average = 
+}
+
+console.log(average); 
---
+++ array/test4.js
@@ -0,0 +1,2 @@
+var array = [5,9,8,12,22,18]
+array.pop
